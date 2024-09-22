aa5959bb0bd9da4e995130f9f78bab757e36d896
lazygarde <dohuong0422@gmail.com>
2024-09-23 00:39:56 +0700

    ActivityResultContract way

--- app/src/main/java/dev/lazygarde/activity/result/MainActivity.kt
+++ app/src/main/java/dev/lazygarde/activity/result/MainActivity.kt
@@ -1,11 +1,15 @@
 package dev.lazygarde.activity.result
 
 import android.os.Bundle
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import dev.lazygarde.activity.result.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private val activityContract = registerForActivityResult(ActivityResultContracts.GetContent()) {
+        binding.iv.setImageURI(it)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -13,7 +17,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.btnSelectImage.setOnClickListener {
-
+            activityContract.launch("image/*")
         }
     }
 }
