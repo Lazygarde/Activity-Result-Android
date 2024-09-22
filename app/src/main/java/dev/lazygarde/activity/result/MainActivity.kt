579c26e6ac23085e51305592a8594bb5d69b4935
lazygarde <dohuong0422@gmail.com>
2024-09-23 00:36:58 +0700

    onActivityResult way

--- app/src/main/java/dev/lazygarde/activity/result/MainActivity.kt
+++ app/src/main/java/dev/lazygarde/activity/result/MainActivity.kt
@@ -1,5 +1,6 @@
 package dev.lazygarde.activity.result
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import dev.lazygarde.activity.result.databinding.ActivityMainBinding
@@ -13,7 +14,26 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.btnSelectImage.setOnClickListener {
+            val intent = Intent(Intent.ACTION_GET_CONTENT)
+                .addCategory(Intent.CATEGORY_OPENABLE)
+                .setType("image/*")
 
+            @Suppress("DEPRECATION")
+            startActivityForResult(intent, GET_IMAGE_REQUEST_CODE)
         }
+    }
+
+    @Deprecated("DEPRECATION")
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == GET_IMAGE_REQUEST_CODE && resultCode == RESULT_OK) {
+            data?.data?.let { uri ->
+                binding.iv.setImageURI(uri)
+            }
+        }
+    }
+
+    companion object {
+        private const val GET_IMAGE_REQUEST_CODE = 100
     }
 }
